8925c743a70202f746cd41618c79e135f9e13198
hansj <hanblues@gmail.com>
2020-11-14 11:20:52 +0900

    'main_map'

--- socarClone/src/screens/MainScreen/index.js
+++ socarClone/src/screens/MainScreen/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { UrlWebview, HtmlWebView } from '../../components/RWebView';
-import sytles from './styles'
+import { View } from 'react-native';
+import NaverMapView, { Circle, Marker, Path, Polyline, Polygon } from "react-native-nmap";
 
-const MainScreen = () => (
+const MainScreen = () => {
 
-    <View>
+    const P0 = { latitude: 37.564362, longitude: 126.977011 };
+    const P1 = { latitude: 37.565051, longitude: 126.978567 };
+    const P2 = { latitude: 37.565383, longitude: 126.976292 };
+
+
+    return (
+
         <View>
-            <Text>메인</Text>
+            <NaverMapView
+                style={{ width: '100%', height: '100%' }}
+                showsMyLocationButton={true}
+                center={{ ...P0, zoom: 16 }}
+                onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+                onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+                onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
+                <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+            </NaverMapView>
         </View>
-    </View>
-)
+    )
+}
 
 export default MainScreen;
